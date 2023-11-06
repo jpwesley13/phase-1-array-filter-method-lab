bbd94e91a1de58914680e958ac4dd3a481d0a86b
Jared Patterson <jpwesley34@gmail.com>
2023-11-06 14:31:35 -0800

    matches drivers

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(drivers, cb){
+    const returnedDrivers = drivers.filter(function(driver){
+        return cb.toLowerCase() === driver.toLowerCase()
+    })
+    return returnedDrivers;
+}
+
+function fuzzyMatch(drivers, string){
+    let letters = string.length
+    const tDrivers = drivers.filter(
+        function(firstTwo){
+            return string === firstTwo.substring(0,letters)
+    })
+    return tDrivers
+}
+
+function matchName(drivers, match){
+    const matchingDrivers = drivers.filter(
+        function(driver){
+            return match === driver.name;
+        }
+    )
+    return matchingDrivers;
+}
